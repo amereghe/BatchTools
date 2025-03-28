7a47017e13f9defd39b84a3a08fd031186e6e113
Alessio Mereghetti <amereghe@Ubuntu-20.04-LTS>
2025-03-28 10:00:14 +0100

    adding an echo on terminal about gzipping `.out`/`.err`/`.log` in `FLUKA/job.sh`

--- user/templates/FLUKA/job.sh
+++ user/templates/FLUKA/job.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export FLUPRO=/usr/local/FLUKA/INFN/2021.2.9
+export FLUPRO=/usr/local/FLUKA_INFN/2024.1.3
 export FLUKA=${FLUPRO}
 export FLUFOR=gfortran
 FLUKAexe=${FLUPRO}/flukahp
@@ -12,15 +12,19 @@ echo " starting job at `date` in folder $PWD as $USER on `hostname`..."
 
 # run
 for inputFile in `ls -1 *.inp` ; do
-        echo "running command: ${FLUKA}/flutil/rfluka -e ${FLUKAexe} -N${NN} -M${MM} ${inputFile%.inp}"
-        ${FLUKA}/flutil/rfluka -e ${FLUKAexe} -N${NN} -M${MM} ${inputFile%.inp}
-        gzip ${inputFile%.inp}???.err ${inputFile%.inp}???.out ${inputFile%.inp}???.log
+    echo "running command: ${FLUKA}/flutil/rfluka -e ${FLUKAexe} -N${NN} -M${MM} ${inputFile%.inp}"
+    ${FLUKA}/flutil/rfluka -e ${FLUKAexe} -N${NN} -M${MM} ${inputFile%.inp}
+    echo ""
+    for ext in err out log ; do
+        echo "gzipping ${inputFile%.inp}???.${ext}..."
+        gzip ${inputFile%.inp}???.${ext}
+    done
 done
 
 # post-processing (modify this part according to your needs)
 for iUnit in $(seq 21 22) ; do
-        echo "gzipping fort.${iUnit} files..."
-        gzip *fort.${iUnit}
+    echo "gzipping fort.${iUnit} files..."
+    gzip *fort.${iUnit}
 done
 
 # end of job

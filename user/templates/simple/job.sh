0a37bc8293be1ab081c41cc463ab8bea92851259
amereghe <alessio.mereghetti@cnao.it>
2026-06-24 08:52:51 +0200

    removing cd $HOME from job.sh (simple template)

--- user/templates/simple/job.sh
+++ user/templates/simple/job.sh
@@ -3,17 +3,14 @@
 # A.Mereghetti, 2022-11-28
 # this is a template batch job file
 
-RunFolder=$HOME
-cd ${RunFolder}
-
 # start batch job
 echo " starting job at `date` in folder $PWD as $USER on `hostname`..."
 
 # --------------------------------------
 # actual simulation job
-for ((ii=1;ii<=360;ii++)); do
+for ((ii=1;ii<=500;ii++)); do
     echo "Hello world! ${ii}"
-    sleep 10s
+    sleep 1s
 done
 # --------------------------------------
 

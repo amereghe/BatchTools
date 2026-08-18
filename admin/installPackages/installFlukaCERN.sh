19677e5beaac8dfe99cbfd1760ba9bf20c4703ae
amereghe <alessio.mereghetti@cnao.it>
2026-08-18 15:57:08 +0200

    separating installation scripts for INFN and CERN FLUKA/flair

---
+++ admin/installPackages/installFlukaCERN.sh
@@ -0,0 +1,48 @@
+#!/usr/bin/bash
+
+# run as root!
+
+FLUKAverLong=4-5.2
+lCopy=true
+lSCopy=false
+lClean=false
+FLUKApath=FLUKA/CERN
+
+die() {
+  echo >&2 "$1"
+  exit $E_BADARGS
+}
+
+# go to suitable folder
+cd /usr/local
+mkdir -p ${FLUKApath}/${FLUKAverLong}
+cd ${FLUKApath}/${FLUKAverLong}
+
+if ${lCopy} ; then
+    echo "copying files..."
+    cp /mnt/san_data/soft/FLUKA/CERN/fluka-${FLUKAverLong}.x86-Linux-gfor9.tgz .
+elif ${lSCopy} ; then
+    echo "scopying files..."
+    scp amereghe@svpvclus01.cnao.group:/mnt/san_data/soft/FLUKA/CERN/fluka-${FLUKAverLong}.x86-Linux-gfor9.tgz .
+fi
+
+# untar material
+tar xvzf fluka-${FLUKAverLong}.x86-Linux-gfor9.tgz
+mv fluka${FLUKAverLong} ${FLUKAverLong}
+
+# compile
+cd ${FLUKAverLong}/src
+make
+cd -
+
+# make installation available for the linux group fluka
+cd ../../../
+chown -R root:fluka FLUKA
+cd -
+
+# clean away package files
+! ${lClean} || rm fluka${FLUKAver}*tgz
+
+# ls
+ls -ltrh --color=auto ${FLUKAverLong}/bin
+ls -ltrh --color=auto ${FLUKAverLong}
